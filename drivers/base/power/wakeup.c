8e9df08b9b6388de3f56e9833c3b62612ac894c7
CheckYourScreen <nimitmehta95@gmail.com>
2017-05-06 14:53:22 +0200

    wakeup: block NETLINK wakelock
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -32,6 +32,10 @@ static bool enable_bluedroid_timer_ws = false;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
 static bool enable_bluesleep_ws = false;
 module_param(enable_bluesleep_ws, bool, 0644);
+static bool enable_msm_hsic_ws = false;
+module_param(enable_msm_hsic_ws, bool, 0644);
+static bool enable_netlink_ws = false;
+module_param(enable_netlink_ws, bool, 0644);
 
 #include "power.h"
 
@@ -502,6 +506,10 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 			!strcmp(ws->name, "wlan_ctrl_wake")) ||
 		(!enable_wlan_wake_ws &&
 			!strcmp(ws->name, "wlan_wake")) ||
+		(!enable_netlink_ws &&
+			!strcmp(ws->name, "NETLINK")) ||
+		(!enable_msm_hsic_ws &&
+			!strcmp(ws->name, "msm_hsic_host")) ||
 		(!enable_bluedroid_timer_ws &&
 			!strcmp(ws->name, "bluedroid_timer"))||
 		(!enable_bluesleep_ws && !strcmp(ws->name, "bluesleep")))) {
